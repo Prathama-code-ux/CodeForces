523c10598c4fa9142ec1230b080a94ecb3aba7a5
Pratham-code-ux <prathama.6327@gmail.com>
2026-05-16 20:50:35 +0530

    football

---
+++ 800/football.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int count = 1;
+
+    string str;
+    cin >> str;
+
+    for(int i = 0; i < str.length(); i++){
+        if(str[i] == str[i-1]){
+            count++;
+        }else{
+            count = 1;
+        }
+''
+        if(count == 7){
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+
+    cout << "NO" << endl;
+
+    return 0;
+}
