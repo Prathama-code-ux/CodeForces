3f0229a09c3116aeb1fb10036cab8b3dc48070b0
Pratham-code-ux <prathama.6327@gmail.com>
2026-05-18 16:57:18 +0530

    Kefa and first steps

---
+++ 900/kefaandfirststeps.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int a[n];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    int count = 1;
+    int maxcount = 1;
+
+    for(int i = 1; i < n; i++){
+        if(a[i] >= a[i-1]){
+            count++;
+        }else{
+            count = 1;
+        }
+        maxcount = max(maxcount, count);
+    }
+
+    cout << maxcount << endl;
+    return 0;
+}
